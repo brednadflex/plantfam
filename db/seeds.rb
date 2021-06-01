51ecf26058e34b182c82f082f2dd852df5980d4d
Julian Thompson <j.thompson@hotmail.de>
2021-06-01 16:28:14 +0200

    added images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ claudiu = User.create!(email: "claudiu@claudiu.com", password: "claudiu123")
 profile_claudiu = Profile.new(
   first_name: "Claudiu",
   last_name: "Florin Popa",
+  profile_img: "https://avatars.githubusercontent.com/u/81229662?v=4",
   description: "I'm in love with plants and enjoy taking care of them.",
   experience: "Plant Whisperer",
   avg_rating: 4.8,
@@ -25,6 +26,7 @@ profile_barney = Profile.new(
   first_name: "Barney",
   last_name: "Haas",
   description: "Plants are just the best and I just can't hide it.",
+  profile_img: "https://avatars.githubusercontent.com/u/77109548?v=4",
   experience: "Plant Whisperer",
   avg_rating: 4.5,
   address: Faker::Address.street_address,
@@ -41,6 +43,7 @@ profile_jal= Profile.new(
   first_name: "Jalynn",
   last_name: "Ridley",
   description: "I take care of my plants as if they were my own children. Let's have a plant chat!",
+  profile_img: "https://avatars.githubusercontent.com/u/72085091?v=4",
   experience: "Seedling",
   avg_rating: 4.6,
   address: Faker::Address.street_address,
@@ -57,8 +60,9 @@ profile_julian = Profile.new(
   first_name: "Julian",
   last_name: "Thompson",
   description: "A plant? What the hell is even that?",
+  profile_img: "https://avatars.githubusercontent.com/u/80887245?s=400&u=a2a1d4d27a7a628a5eebb5fa888fe55fbaa6dd00&v=4",
   experience: "Seedling",
-  avg_rating: 3.5,
+  avg_rating: 3.9,
   address: Faker::Address.street_address,
   sitter: true,
   advisor: true
@@ -77,7 +81,7 @@ puts "Creating 50 sample users with profiles..."
   profile_new_user = Profile.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  profile_img: Faker::Avatar.image,
+  profile_img: "https://thispersondoesnotexist.com/",
   description: Faker::ChuckNorris.fact,
   experience: ["Seedling", "Plant Friend", "Moss Person", "Plant Whisperer"].sample,
   avg_rating: (1..4).to_a.sample + [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9].sample,
