7e40e5b13ef1ed911ec500ce18519ba07722b661
Jalynn Ridley <jallywally@gmail.com>
2021-06-02 09:01:08 +0200

    finished update profile form and show one profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,10 @@ class User < ApplicationRecord
   has_many :bookings_as_client, class_name: "Booking", foreign_key: :client_id
   has_many :bookings_as_provider, class_name: "Booking", foreign_key: :provider_id
 
-  # after the user is created, it automatically creates the profile with that inputted info
-  after_create :create_profile!
+  # after the user is created, it automatically creates the profile with that inputed info
+
+  after_create :init_profile
   def init_profile
     create_profile!
   end
-  
 end
