c0c2cefac6ee7bfe5f60d68231f3fd369d931b12
Bernard Haas <barney.haas@gmail.com>
2021-06-02 17:11:53 +0200

    chat continued

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :index, :new, :create ]
   end
 
-  resources :chat_rooms, only: :show do
+  resources :chat_rooms, only: [:index, :show] do
     resources :messages, only: :create
   end
 
