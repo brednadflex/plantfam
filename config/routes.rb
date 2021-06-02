fcbd5e5b2599a984b9c4b8254015efb2f21519e6
Bernard Haas <barney.haas@gmail.com>
2021-06-02 15:21:01 +0200

    basic chat feature implemented

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   resources :profiles, only: [ :index, :show, :new, :edit, :update ] do
     resources :bookings, only: [ :index, :new, :create ]
   end
+
+  resources :chat_rooms, only: :show do
+    resources :messages, only: :create
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
