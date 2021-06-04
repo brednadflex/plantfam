4ec77ae34a1749624be58bd8c69bb414596f9376
Bernard Haas <barney.haas@gmail.com>
2021-06-04 11:59:18 +0200

    adding start and end date validation for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :client, class_name: "User"
   belongs_to :provider, class_name: "User"
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
