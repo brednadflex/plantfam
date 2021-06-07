172e6409ce5732b41054ddb38fdeb718ff5cf4cf
Jalynn Ridley <jallywally@gmail.com>
2021-06-07 13:01:16 +0200

    added stii_price and advisor_price to profiles migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_160242) do
+ActiveRecord::Schema.define(version: 2021_06_07_105838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2021_06_03_160242) do
     t.boolean "public", default: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "sitter_price"
+    t.string "advisor_price"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
