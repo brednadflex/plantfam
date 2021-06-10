74bffc9215792735068f4d70ff62f906b172fd50
Bernard Haas <barney.haas@gmail.com>
2021-06-10 12:26:50 +0200

    implemented collapsible

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def chatroom_finder(user1, user2)
+    requester1 = User.find(user1.id)
+    receiver1 = User.find(user2.id)
+    chatroom1 = ChatRoom.where(requester: requester1, receiver: receiver1).take
+
+    requester2 = User.find(user2.id)
+    receiver2 = User.find(user1.id)
+    chatroom2 = ChatRoom.where(requester: requester2, receiver: receiver2).take
+
+    if chatroom1.nil? && chatroom2.nil?
+      chatroom = ChatRoom.create(requester: requester1, receiver: requester2)
+      flash.delete(:notice)
+    elsif chatroom1.nil?
+      chatroom = chatroom2
+    elsif chatroom2.nil?
+      chatroom = chatroom1
+    end
+
+    flash.delete(:notice)
+    return chatroom
+  end
 end
